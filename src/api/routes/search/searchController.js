bf4836e440e6cbca4d5134b6148289540dd34dae
Hector Feliz Recio <7598052+hectorfeliz@users.noreply.github.com>
2021-04-22 19:42:26 -0400

    Update searchController.js

--- src/api/routes/search/searchController.js
+++ src/api/routes/search/searchController.js
@@ -4,14 +4,16 @@ require('dotenv').config({
 
 const request = require('request'); 
 
-exports.getSymbolSearch = async (query) => {
- 
-    return new Promise((resolve, reject) => { 
+exports.getSymbolSearch = async (req, res, next) => {
+  try {
+    console.log('Query is...');
+    console.log(req);
+
 
     const options = {
         method: 'GET',
         url: 'https://twelve-data1.p.rapidapi.com/symbol_search',
-        qs: {symbol: query.query, outputsize: '30'},
+        qs: {symbol: req.query, outputsize: '30'},
         headers: {
           'x-rapidapi-key': process.env.RAPID_API_KEY,
           'x-rapidapi-host': 'twelve-data1.p.rapidapi.com',
@@ -19,28 +21,21 @@ exports.getSymbolSearch = async (query) => {
         }
       };
       
-  
-      // Get whole Response object.
-    const response = request(options);
-
-    // Returns the Promise.Resolve or Reject based on response.
-    if (response.statusCode < 200 || response.statusCode > 300) {
-      const errorMsg = 'Error occurred while POSTing the request. Got status: ' + response.status;
-      console.error(errorMsg);
-
-      // Reject the promise. Should be caught.
-      return reject(errorMsg);
-    }
-
-    const responseBody = response.body;
-    console.log(responseBody);
-
-    // Return the response.
-    return resolve(responseBody);
-
-  })
+      request(options, function (error, response, body) {
+          if (error) throw new Error(error);
     
-  
+          console.log('body..');
+          console.log(body);
+          return body;
+          
+       
+      });
+
+
+    
+  } catch (err) {
+    throw err;
+  }
 };
 
 
