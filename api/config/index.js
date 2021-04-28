96336c1f30b7bce65d4ff4df208d36c849836a3d
Hector Feliz Recio <7598052+hectorfeliz@users.noreply.github.com>
2021-04-27 22:09:00 -0400

    Update index.js

--- api/config/index.js
+++ api/config/index.js
@@ -1,3 +1,3 @@
-exports.DATABASE_URI = process.env.DATABASE_URI || 'mongodb://localhost:27017/stocks-app'
+exports.DATABASE_URL = process.env.DATABASE_URI || 'mongodb://localhost:27017/stocks-app'
 exports.PORT = process.env.PORT || '8080'
 exports.RAPID_API_KEY = process.env.RAPID_API_KEY || '89c20ee19cmshb9b1a97ffa75f6bp13b9a9jsn1a1284be8d95'
